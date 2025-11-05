e6984fbb03eb2c0785c12e7326235e36c9426747
Dmitrii Frolov <steel835@gmail.com>
2025-11-06 00:27:01 +0400

    Moved Assertion exception out

--- src/TestClientResponse/String/TestStringResponseAssertionException.cs
+++ src/TestClientResponse/String/TestStringResponseAssertionException.cs
@@ -1,34 +1,4 @@
-﻿using TestClientResponse.String;
-
-namespace TestClientResponse;
-
-public abstract class TestResponseAssertionException<TResponse> : TestResponseException
-    where TResponse : TestResponse
-{
-    protected readonly TResponse Response;
-
-    public TestResponseAssertionException(TResponse response, string message) : base(message)
-    {
-        Response = response;
-    }
-
-    public TestResponseAssertionException(TResponse response, string message, Exception inner) : base(message, inner)
-    {
-        Response = response;
-    }
-
-    public override string Message => $"{BuildAssertMessage(base.Message)}";
-
-    protected virtual string BuildAssertMessage(string message)
-    {
-        var result = $"""
-            {message}
-            Status code: {(int)Response.StatusCode} ({Response.HttpResponse.ReasonPhrase})
-            """;
-        
-        return result;
-    }
-}
+﻿namespace TestClientResponse.String;
 
 public class TestStringResponseAssertionException : TestResponseAssertionException<TestStringResponse>
 {
@@ -43,7 +13,6 @@ public class TestStringResponseAssertionException : TestResponseAssertionExcepti
             Response: 
             {(Response.IsRead ? Response.AsString : "*not read*")}
             """;
-        // TODO, TEST: read, not read 
 
         return result;
     }
---
+++ src/TestClientResponse/TestResponseAssertionException.cs
@@ -0,0 +1,29 @@
+﻿namespace TestClientResponse;
+
+public abstract class TestResponseAssertionException<TResponse> : TestResponseException
+    where TResponse : TestResponse
+{
+    protected readonly TResponse Response;
+
+    public TestResponseAssertionException(TResponse response, string message) : base(message)
+    {
+        Response = response;
+    }
+
+    public TestResponseAssertionException(TResponse response, string message, Exception inner) : base(message, inner)
+    {
+        Response = response;
+    }
+
+    public override string Message => $"{BuildAssertMessage(base.Message)}";
+
+    protected virtual string BuildAssertMessage(string message)
+    {
+        var result = $"""
+            {message}
+            Status code: {(int)Response.StatusCode} ({Response.HttpResponse.ReasonPhrase})
+            """;
+        
+        return result;
+    }
+}
