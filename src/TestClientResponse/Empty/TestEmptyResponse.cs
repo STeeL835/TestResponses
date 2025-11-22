504b712e97b2fb0d99359f6b336369af8a5432db
Dmitrii Frolov <steel835@gmail.com>
2025-11-22 23:31:49 +0400

    First implementation of EmptyResponse

---
+++ src/TestClientResponse/Empty/TestEmptyResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace TestClientResponse.Empty;
+
+public record TestEmptyResponse(HttpResponseMessage HttpResponse) : TestResponse(HttpResponse)
+{
+    protected override Task ReadResponse() => Task.CompletedTask;
+
+    public override string ToString() => $"""
+        {TestResponseFormatter.FormatStatusCodeInfo(this)}
+        Response: *empty*
+        """;
+    
+    // TODO, TEST: What if response is a stream or text 
+    // TODO, TEST: Check the response type header?
+}
