9d01fc0204f20c64bbf7330faf4253f76860923e
Dmitrii Frolov <steel835@gmail.com>
2025-12-04 01:28:16 +0400

    ShouldSucceed for every type

---
+++ src/TestResponses/Types/Empty/TestEmptyResponseExtensions.cs
@@ -0,0 +1,14 @@
+﻿using TestResponses.Features;
+
+namespace TestResponses.Empty;
+
+public static class TestEmptyResponseExtensions 
+{
+    public static async Task ShouldSucceed<T>(this Task<TestEmptyResponse> responseTask, UniStatusCode? withStatusCode = null)
+    {
+        var testResponse = await responseTask;
+        await testResponse.Read();
+        
+        testResponse.AssertValid(withStatusCode);
+    }
+}
--- src/TestResponses/Types/Json/TestJsonResponseExtensions.cs
+++ src/TestResponses/Types/Json/TestJsonResponseExtensions.cs
@@ -1,13 +1,15 @@
-﻿namespace TestResponses.Json;
+﻿using TestResponses.Features;
+
+namespace TestResponses.Json;
 
 public static class TestJsonResponseExtensions 
 {
-    public static async Task<T?> ShouldSucceed<T>(this Task<TestJsonResponse<T>> responseTask)
+    public static async Task<T?> ShouldSucceed<T>(this Task<TestJsonResponse<T>> responseTask, UniStatusCode? withStatusCode = null)
     {
         var testResponse = await responseTask;
         await testResponse.Read();
         
-        testResponse.AssertStatusCode(200..299); // TODO: pass withstatuscode instead of hard-code
+        testResponse.AssertValid(withStatusCode);
 
         return testResponse.AsDto;
     }
