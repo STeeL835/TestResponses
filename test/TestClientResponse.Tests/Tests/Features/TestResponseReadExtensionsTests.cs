a09d108cd3eaddfdc3c8a772d4ad21daa7d36179
Dmitrii Frolov <steel835@gmail.com>
2025-12-03 22:35:44 +0400

    Added test for ReadAs extension

--- test/TestClientResponse.Tests/Tests/Features/TestResponseReadExtensionsTests.cs
+++ test/TestClientResponse.Tests/Tests/Features/TestResponseReadExtensionsTests.cs
@@ -15,15 +15,26 @@ public class TestResponseReadExtensionsTests
         var testResponse = await getResponse().ReadAs<TestTextResponse>();
 
         testResponse.IsRead.Should().BeTrue();
+        testResponse.ExpectedStatusCode.Should().BeNull();
     }
     
-    record AdvancedTextResponse(HttpResponseMessage HttpMessage, string Text) : TestTextResponse(HttpMessage);
+    [Fact]
+    public async Task ReadAs_WithExpectedStatusCode_ShouldReturnReadTestResponse()
+    {
+        var getResponse = () => TestHttpClient.ReceiveResponse(r => r.Respond(MediaTypeNames.Text.Plain, "text"));
+
+        var testResponse = await getResponse().ReadAs<TestTextResponse>(expectedStatusCode: 200);
+
+        testResponse.IsRead.Should().BeTrue();
+        testResponse.ExpectedStatusCode!.Range.Should().Be(200..200);
+    }
+
     [Fact]
     public async Task ReadAs_DoesNotHaveCorrectCtor_ShouldThrowException()
     {
         var getResponse = () => TestHttpClient.ReceiveResponse(r => r.Respond(MediaTypeNames.Text.Plain, "text"));
 
-        var action = () => getResponse().ReadAs<AdvancedTextResponse>();
+        var action = () => getResponse().ReadAs<InvalidCtorResponse>();
 
         await action.Should().ThrowAsync<TestResponseException>();
     }
