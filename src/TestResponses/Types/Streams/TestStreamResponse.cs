a0de42b8bbc0c88a5b371dd5e9fef3f9b81e5335
Dmitrii Frolov <steel835@gmail.com>
2025-12-06 02:09:34 +0400

    Stream response type

---
+++ src/TestResponses/Types/Streams/TestStreamResponse.cs
@@ -0,0 +1,22 @@
+﻿using TestResponses.Features;
+
+namespace TestResponses.Streams;
+
+public class TestStreamResponse(HttpResponseMessage httpResponse) : TestResponse(httpResponse)
+{
+    private ResponseValue<Stream>? _stream;
+
+    public Stream AsStream => _stream.GetOrThrow()!;
+
+    protected override async Task ReadResponse()
+    {
+        _stream = await ResponseValue.Create(this, HttpResponse.Content.ReadAsStreamAsync);
+    }
+
+    internal override bool CanHandleContent() => ContentType is not null && HttpResponse.Content is StreamContent;
+
+    protected override string GetInfoString() => $"""
+        {TestResponseFormatter.FormatStatusCodeInfo(this)}
+        Response: {ContentType} stream {AsStream.Length} bytes long
+        """;
+}
